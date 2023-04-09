0785193108baa1feab25f6d23b809e9f9139096a
Cristian Cuccorese <110430430+cristiancuccorese@users.noreply.github.com>
2023-04-09 19:27:57 -0300

    Initial commit

---
+++ Ejercicio1/untitled/src/main/java/org/openBootcamp/Ejercicio1.java
@@ -0,0 +1,20 @@
+package org.openBootcamp;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String nombre = "Cristian";
+        int edad =35;
+        long tel =454567;
+        double altura = 1.75;
+        boolean esAregentino =true;
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Teléfono: " + tel);
+        System.out.println("Es Argentino: " + esAregentino);
+
+
+
+
+    }
+}
