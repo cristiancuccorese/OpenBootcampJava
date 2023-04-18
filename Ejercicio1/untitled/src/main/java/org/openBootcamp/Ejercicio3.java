7ea6016fc865a84553c980198ced25a2cc704f3b
Cristian Cuccorese <110430430+cristiancuccorese@users.noreply.github.com>
2023-04-17 21:23:26 -0300

    Ejercicio3

---
+++ Ejercicio1/untitled/src/main/java/org/openBootcamp/Ejercicio3.java
@@ -0,0 +1,13 @@
+package org.openBootcamp;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        String[] textos = {"Gran", "Oso", "de", "la", "montaña", "Gris" };
+
+        for (int i = 0; i < textos.length; i++) {
+            System.out.print(textos[i] + " ");
+        }
+
+    }
+}
